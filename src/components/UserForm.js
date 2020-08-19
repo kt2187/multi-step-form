3acc26e7ccf2e41381061a48a8cffc76b26372ef
Katie Barriere <kbarriere@gmail.com>
2020-08-19 15:55:26 -0400

    Add initial form state and methods to user form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
 
 export class UserForm extends Component {
   state = {
@@ -11,9 +12,42 @@ export class UserForm extends Component {
     bio: "",
   };
 
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step + 1 });
+  };
+
+  // Go back to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step - 1 });
+  };
+
+  // Handle fields change
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
   render() {
-    return <div></div>;
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={this.values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
   }
 }
-
 export default UserForm;
