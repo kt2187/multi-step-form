b73b04450e79543a99291e7e56f08d605c3b7780
Katie Barriere <kbarriere@gmail.com>
2020-08-19 20:24:43 -0400

    Add confirmationform to confirm user data

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -25,7 +25,7 @@ export class FormUserDetails extends Component {
           <TextField
             hintText="Enter Your Last Name"
             floatingLabelText="Last Name"
-            onChange={handleChange("LastName")}
+            onChange={handleChange("lastName")}
             defaultValue={values.lastName}
           />
           <br />
